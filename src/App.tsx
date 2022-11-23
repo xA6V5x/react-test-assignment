f773ada30b8299df7d17922cb43384600ec8aa8d
xA6V5x <ojedaagustinmanuel@gmail.com>
2022-11-23 17:57:22 -0300

    company logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import { useState } from 'react';
 import Login from './components/login/login';
+import CompanyLogo from './components/logo/logo';
 import Profile from './components/profile/profile';
 import { profileProps } from './types/profile';
 
 function App() {
      let userInitial = { name: '', avatar: '' };
 
-     const [user, setUser] = useState(userInitial);
+     const [user, setUser] = useState<profileProps>(userInitial);
 
      const handleSetState = (user: profileProps) => {
           setUser(user);
      };
 
-     //  return user ? <div>Profile TBD</div> : <div>Login TBD</div>;
-
-     return user.name != '' ? (
-          <Profile name={user.name} avatar={user.avatar} handleSetState={handleSetState} />
-     ) : (
-          <Login handleSetState={handleSetState} />
+     return (
+          <div className="appContainer">
+               <CompanyLogo />
+               {user.name != '' && user.avatar != '' ? (
+                    <Profile
+                         name={user.name}
+                         avatar={user.avatar}
+                         handleSetState={handleSetState}
+                    />
+               ) : (
+                    <Login handleSetState={handleSetState} />
+               )}
+          </div>
      );
 }
 
